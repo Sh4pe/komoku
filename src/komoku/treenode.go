cb5a27a0aa86e36d15fb2d1f28f1932d877aa7d9
David Nies <nies.david@googlemail.com>
2010-08-01 23:33:08 +0200

    Added a LICENSE file and inserted license notes everywhere.

--- src/komoku/treenode.go
+++ src/komoku/treenode.go
@@ -1,3 +1,10 @@
+/* 
+ * (c) 2010 by David Nies (nies.david@googlemail.com)
+ *     http://www.twitter.com/Sh4pe
+ *
+ * Use of this source code is governed by a license 
+ * that can be found in the LICENSE file.
+ */
 package komoku
 
 import (
