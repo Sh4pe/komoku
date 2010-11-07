95111b223fc6d8877825017b444134a549967e67
David Nies <nies.david@googlemail.com>
2010-11-07 17:02:34 +0100

    Some refactoring of the buildsystem

---
+++ src/common.mk
@@ -0,0 +1,17 @@
+# vim: set noexpandtab :
+
+#
+# (c) 2010 by David Nies (nies.david@googlemail.com)
+#     http://www.twitter.com/Sh4pe
+#
+# Use of this source code is governed by a license 
+# that can be found in the LICENSE file.
+#
+
+# These are settings shared by all makefiles of this project
+
+CC=6g
+LINK=6l -e
+PACK=gopack
+PROFILER=sudo 6prof
+GOPPROF=gopprof
